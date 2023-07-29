b812c9f3b212ac3b4cbcaba60b8dd607cfd0e330
blackt3a <1664102260@qq.com>
2023-07-29 17:41:04 +0800

    7/29/23 first

---
+++ 数据结构/code/ListTest.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+struct Node {
+	int number;
+	char *str;
+}
+
+
+typedef Node[100] List;
+
+
+
+void InitList(&L);
+
+int Length(L);
+
+int LocateElem(L,e);
+
+
+
+int main()
+{
+
+
+
+
+
+
+
+
+}
